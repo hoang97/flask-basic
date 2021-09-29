511aa7090f6b4f052925a5fbb2b578f22c99a8a2
unknown <hoangnd997@gmail.com>
2021-09-29 21:10:57 +0700

    delete config and test files

--- config.py
+++
@@ -1,7 +0,0 @@
-USER_DB_DIR = 'database/logindb.txt'
-MESSAGE_DB_DIR = 'database/messagedb.txt'
-IMAGES_ROOT = 'images/'
-SECRET_KEY = 'bimatvclluon'
-DATABASE_URI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000'
-UPLOAD_FOLDER = 'images'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
